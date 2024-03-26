da0d66d5fa32d5bc17599567541b634c7d8750bb
Ao Wang <127380378+AoWangg@users.noreply.github.com>
2024-03-26 16:41:10 +0800

    Update BotNFT.sol

--- BotNFT.sol
+++ BotNFT.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BotNFT is ERC721URIStorage {
+contract BigitBotNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
@@ -30,7 +30,7 @@ contract BotNFT is ERC721URIStorage {
       bool sold
     );
 
-    constructor() ERC721("Bigit", "BGT") {
+    constructor() ERC721("Bigit BOT", "BGBT") {
       owner = payable(msg.sender);
     }
 
